753d14980e0d318d56625c668d62d4a790a1f51b
littleman74 <43424115+littleman74@users.noreply.github.com>
2018-09-24 08:48:04 -0400

    initial

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var myImage = document.querySelectorAll('img')[0]
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src')
+	if(mySrc === 'images/gopher.png'){
+		myImage.setAttribute('src','images/pope-gopher.png')
+	}else{
+		myImage.setAttribute('src','images/gopher.png')
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelectorAll('h1')[1];
+
+function setUserName(){
+	var myName = prompt('Pwees enta ur name uwu');
+	localStorage.setItem('name',myName);
+	myHeading.innerHTML = 'Mykale welcomes '+myName;
+}
+
+if(!localStorage.getItem('name')){
+	setUserName();
+}else{
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Do not worry '+storedName+' mykale welcomes you';
+}
+
+
+
+myButton.onclick = function() {
+  setUserName();
+}
+
