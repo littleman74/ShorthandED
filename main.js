5cb266ceb62504c1744661dfcf08bfaf836fc349
Michael M <easypete@Michaels-MacBook-Pro.local>
2018-10-18 16:54:28 -0400

    Chrome compatibility

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import {
-    MDCList
-} from "@material/list";
+import {MDCList} from "@material/list";
 const list = MDCList.attachTo(document.querySelector('.mdc-list'));
 list.wrapFocus = true;
 import {MDCTopAppBar} from '@material/top-app-bar/index';
@@ -8,38 +6,6 @@ import {MDCTopAppBar} from '@material/top-app-bar/index';
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
-/*var myImage = document.querySelectorAll('img')[0]
 
-myImage.onclick = function(){
-	var mySrc = myImage.getAttribute('src')
-	if(mySrc === 'images/gopher.png'){
-		myImage.setAttribute('src','images/pope-gopher.png')
-	}else{
-		myImage.setAttribute('src','images/gopher.png')
-	}
-}
-
-// Personalized welcome message code
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelectorAll('h1')[1];
-
-function setUserName(){
-	var myName = prompt('Pwees enta ur name uwu');
-	localStorage.setItem('name',myName);
-	myHeading.innerHTML = 'Mykale welcomes '+myName;
-}
-
-if(!localStorage.getItem('name')){
-	setUserName();
-}else{
-	var storedName = localStorage.getItem('name');
-	myHeading.innerHTML = 'Do not worry '+storedName+' mykale welcomes you';
-}
-
-
-
-myButton.onclick = function() {
-  setUserName();
-}
-*/
+var divStudentCards = document.getElementsByClassName("studentcard");
+console.log(divStudentCards[0])
