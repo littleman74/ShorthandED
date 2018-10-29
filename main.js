24fdc0bed54a7e62366f5a588a1137de847d0b37
Michael M <easypete@Michaels-MacBook-Pro-2.local>
2018-10-28 20:19:32 -0400

    idk

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import {MDCList} from "@material/list";
 const list = MDCList.attachTo(document.querySelector('.mdc-list'));
 list.wrapFocus = true;
-import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {
+	MDCTopAppBar
+} from '@material/top-app-bar/index';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 var divStudentCard = document.getElementById("studentcard");
+var cln = divStudentCard.cloneNode(true);
+document.body.appendChild(cln);
